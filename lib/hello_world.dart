c2a5fe006453a03d0a2a43f5b750a1c357524596
v.loitp <v.Loitp@cloudhms.net>
2021-01-13 15:30:06 +0700

    update

--- lib/hello_world.dart
+++ lib/hello_world.dart
@@ -1,38 +1,68 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  print("Hello MyApp");
+  print(1 / 2);
+
+  bool b = 1 == 2;
+  print(b);
+
+  var mapping = {'id': 1, 'name': 'Dart'};
+  print(mapping);
+  print(mapping.keys);
+  print(mapping.values);
+  print(mapping.entries.first);
+  mapping.values.forEach((element) {
+    print(element);
+  });
+  mapping.forEach((key, value) {
+    if (key == "name") {
+      print(value);
+    }
+  });
+
+  print("--------------------------------------------");
+  dynamic a = 123;
+  print(a);
+  a = "One two three";
+  print(a);
+
+  print("--------------------------------------------");
+  for (int i = 0; i < 5; i++) {
+    if (i % 2 == 0) {
+      print("-> " + i.toString());
+    }
+  }
+
+  print("--------------------------------------------");
+  void add(int a, int b) {
+    print("Sum: " + (a + b).toString());
+  }
+
+  add(6, 9);
+
+  print("--------------------------------------------");
+  People p = People("Loitp123456");
+  p.showData();
+
   runApp(MyApp());
+}
+
+class People {
+  String name = "";
+
+  People(String name) {
+    this.name = name;
+  }
+
+  void showData() {
+    print("showData " + this.name);
+  }
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print("Hello MyApp");
-    print(1 / 2);
-
-    bool b = 1 == 2;
-    print(b);
-
-    var mapping = {'id': 1, 'name': 'Dart'};
-    print(mapping);
-    print(mapping.keys);
-    print(mapping.values);
-    print(mapping.entries.first);
-    mapping.values.forEach((element) {
-      print(element);
-    });
-    mapping.forEach((key, value) {
-      if (key == "name") {
-        print(value);
-      }
-    });
-
-    print("--------------------------------------------");
-    dynamic a = 123;
-    print(a);
-    a = "One two three";
-    print(a);
-
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
