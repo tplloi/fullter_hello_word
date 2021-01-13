a74ba25c9af2e53e9a16e669dd4e21f2e081dcca
v.loitp <v.Loitp@cloudhms.net>
2021-01-13 14:57:40 +0700

    update

--- lib/hello_world.dart
+++ lib/hello_world.dart
@@ -7,6 +7,32 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print("Hello MyApp");
+    print(1 / 2);
+
+    bool b = 1 == 2;
+    print(b);
+
+    var mapping = {'id': 1, 'name': 'Dart'};
+    print(mapping);
+    print(mapping.keys);
+    print(mapping.values);
+    print(mapping.entries.first);
+    mapping.values.forEach((element) {
+      print(element);
+    });
+    mapping.forEach((key, value) {
+      if (key == "name") {
+        print(value);
+      }
+    });
+
+    print("--------------------------------------------");
+    dynamic a = 123;
+    print(a);
+    a = "One two three";
+    print(a);
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
