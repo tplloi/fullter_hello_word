2dc33fdfcd7234aada1965cb8654ae76bdf1420e
v.loitp <v.Loitp@cloudhms.net>
2021-01-14 10:57:30 +0700

    update

---
+++ lib/text.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: text2(),
+        ),
+        backgroundColor: Colors.white70,
+      ),
+    );
+  }
+
+  Widget text1() {
+    return Text(
+      'Hello World!\nLoitp1\nLoitp2\nLoitp3',
+      style: TextStyle(fontWeight: FontWeight.bold),
+      maxLines: 5,
+      textAlign: TextAlign.left,
+      textDirection: TextDirection.rtl,
+    );
+  }
+
+  Widget text2() {
+    return Text.rich(
+      TextSpan(
+        children: <TextSpan>[
+          TextSpan(
+              text: "Hello ", style: TextStyle(fontStyle: FontStyle.italic)),
+          TextSpan(
+              text: "World", style: TextStyle(fontWeight: FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+}
