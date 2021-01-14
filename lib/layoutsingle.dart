c4b69b06fdaf8bf1403ed4d96bfe4f6ab5c48c82
v.loitp <v.Loitp@cloudhms.net>
2021-01-14 11:37:30 +0700

    update

---
+++ lib/layoutsingle.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: MyButton(),
+        ),
+        backgroundColor: Colors.white70,
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  MyButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(
+          top: BorderSide(width: 1.0, color: Color(0xFFFFFFFFFF)),
+          left: BorderSide(width: 1.0, color: Color(0xFFFFFFFFFF)),
+          right: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+          bottom: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+        ),
+      ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
+            left: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
+            right: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
+            bottom: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
+          ),
+          color: Colors.grey,
+        ),
+        child: const Text(
+          'OK Loitp',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
