83aa20d5fdc7463b0b24f86626572c0cff8c18a0
v.loitp <v.Loitp@cloudhms.net>
2021-01-18 15:05:00 +0700

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hello_word/Counter.dart';
+import 'file:///D:/LoitppFlutter/hello_word/lib/widget/Counter.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
