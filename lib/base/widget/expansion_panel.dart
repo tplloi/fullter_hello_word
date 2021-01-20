27305f0fd28ec26da61d73a4cc6bbd2ea28aeaf0
v.loitp <v.Loitp@cloudhms.net>
2021-01-20 15:38:26 +0700

    update

---
+++ lib/base/widget/expansion_panel.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyHome(),
+    // Set the theme's primary color, accent color,
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      accentColor: Colors.blue,
+      // Set background color
+      backgroundColor: Colors.blue,
+    ),
+  ));
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // AppBar
+      appBar: AppBar(
+        // AppBar Title
+        title: Text("ExpansionPanelWidget"),
+      ),
+      body: ExpansionPanelWidget(),
+    );
+  }
+}
+
+class ExpansionPanelWidget extends StatefulWidget {
+  @override
+  _ExpansionPanelWidgetState createState() => _ExpansionPanelWidgetState();
+}
+
+class _ExpansionPanelWidgetState extends State<ExpansionPanelWidget> {
+  List<Item> _data = generateItems(8);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Container(
+          child: buildPanel(),
+        ),
+      ],
+    );
+  }
+
+  Widget buildPanel() {
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((Item item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              title: Text(item.headerValue),
+            );
+          },
+          body: ListTile(
+              title: Text(item.expandedValue),
+              subtitle: Text('Click to delete'),
+              trailing: Icon(Icons.delete),
+              onTap: () => setState(() =>
+                  _data.removeWhere((currentItem) => item == currentItem))),
+          isExpanded: item.isExpanded,
+        );
+      }).toList(),
+    );
+  }
+}
+
+class Item {
+  Item({
+    this.expandedValue,
+    this.headerValue,
+    this.isExpanded = false,
+  });
+
+  String expandedValue;
+  String headerValue;
+  bool isExpanded;
+}
+
+List<Item> generateItems(int numberOfItems) {
+  return List.generate(numberOfItems, (int index) {
+    return Item(
+      headerValue: 'ExpansionPanel $index',
+      expandedValue: 'item $index',
+    );
+  });
+}
