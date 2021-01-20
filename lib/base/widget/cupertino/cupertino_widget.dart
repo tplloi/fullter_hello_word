305d53108795606790e7d9467bfea7403e32e13d
v.loitp <v.Loitp@cloudhms.net>
2021-01-20 15:31:30 +0700

    update

--- lib/base/widget/cupertino/cupertino_widget.dart
+++ lib/base/widget/cupertino/cupertino_widget.dart
@@ -33,7 +33,7 @@ void main() {
               SizedBox(
                 height: 50,
               ),
-
+              CupertinoSliderWidget(),
               SizedBox(
                 height: 50,
               ),
@@ -222,3 +222,45 @@ class CupertinoDialogActionWidget extends StatelessWidget {
   }
 }
 
+class CupertinoSliderWidget extends StatefulWidget {
+  @override
+  _CupertinoSliderWidgetState createState() => _CupertinoSliderWidgetState();
+}
+
+class _CupertinoSliderWidgetState extends State<CupertinoSliderWidget> {
+  double _value1 = 20;
+  double _value2 = 15;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CupertinoSlider(
+          value: _value1,
+          min: 0,
+          max: 100,
+          onChanged: (double value) {
+            setState(() => _value1 = value);
+          },
+        ),
+        Text('Slider value ：${_value1.toStringAsFixed(1)}'),
+        CupertinoSlider(
+          value: _value2,
+          divisions: 5,
+          min: 0,
+          max: 100,
+          activeColor: Colors.green,
+          onChanged: (double value) {
+            setState(() {
+              _value2 = value;
+            });
+          },
+          onChangeStart: (v) {},
+          onChangeEnd: (v) {},
+        ),
+        Text('Slider value ：${_value2.toString()}'),
+      ],
+    );
+  }
+}
