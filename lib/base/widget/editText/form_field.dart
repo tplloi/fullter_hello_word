d0a7f061858be5f1ab4a1671b2279b562bff550d
v.loitp <v.Loitp@cloudhms.net>
2021-01-20 16:06:57 +0700

    update

---
+++ lib/base/widget/editText/form_field.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("FormField"),
+      ),
+      body: FormField(),
+    ),
+  ));
+}
+
+class FormField extends StatefulWidget {
+  @override
+  _FormFieldState createState() => _FormFieldState();
+}
+
+class _FormFieldState extends State<FormField> {
+  final formKey = GlobalKey<FormState>();
+  bool isAutoValidate = false;
+  var name;
+  var tel;
+  var email;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Form(
+          key: formKey,
+          autovalidate: isAutoValidate,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Name'),
+                keyboardType: TextInputType.text,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                onSaved: (value) => name = value,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Tel'),
+                keyboardType: TextInputType.phone,
+                validator: (value) {
+                  if (value.length != 11)
+                    return 'Tel Number must be 11 digit';
+                  else
+                    return null;
+                },
+                onSaved: (value) => tel = value,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  Pattern pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regex = new RegExp(pattern);
+                  if (!regex.hasMatch(value))
+                    return 'Not Valid Email';
+                  else
+                    return null;
+                },
+                onSaved: (value) => email = value,
+              ),
+              SizedBox(height: 10),
+              RaisedButton(
+                onPressed: validateInputs,
+                child: Text('Validate'),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  void validateInputs() {
+    if (formKey.currentState.validate()) {
+      formKey.currentState.save();
+    } else {
+      // After input, turn on automatic inspection
+      setState(() => isAutoValidate = true);
+    }
+  }
+
+  Future<bool> willPop() {
+    return showDialog(
+      builder: (context) => AlertDialog(
+        title: Text('Exit the page?'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('No'),
+            onPressed: () => Navigator.pop(context, false),
+          ),
+          FlatButton(
+            child: Text('Yes'),
+            onPressed: () => Navigator.pop(context, true),
+          ),
+        ],
+      ),
+      context: context,
+    );
+  }
+}
