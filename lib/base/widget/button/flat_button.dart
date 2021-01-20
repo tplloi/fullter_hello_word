368923eca4ae7eaa9bac5124f80eea8597a73570
v.loitp <v.Loitp@cloudhms.net>
2021-01-20 14:23:19 +0700

    update

---
+++ lib/base/widget/button/flat_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("RaisedButtonWidget"),
+      ),
+      body: Center(
+        child: FlatButtonWidget(),
+      ),
+    ),
+    // Set the theme's primary color, accent color,
+    theme: ThemeData(
+      primarySwatch: Colors.green,
+      accentColor: Colors.lightGreenAccent,
+      // Set background color
+      backgroundColor: Colors.black12,
+    ),
+  ));
+}
+
+class FlatButtonWidget extends StatefulWidget {
+  @override
+  _FlatButtonWidgetState createState() => _FlatButtonWidgetState();
+}
+
+class _FlatButtonWidgetState extends State<FlatButtonWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      color: Colors.blue,
+      onPressed: () {
+        /*...*/
+      },
+      child: Text(
+        "Flat Button",
+      ),
+    );
+  }
+}
