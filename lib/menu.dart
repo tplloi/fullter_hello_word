98295152d639f0ee154883b79abaefd1acf14063
Loitp <v.loitp@cloudhms.net>
2021-02-19 12:55:56 +0700

    update

---
+++ lib/menu.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MenuWidget());
+}
+
+class MenuWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Menu"),
+        ),
+        body: Center(
+          child: Image.network(
+            'https://flutter.dev/assets/flutter-lockup-1caf6476beed76adec3c477586da54de6b552b2f42108ec5bc68dc63bae2df75.png',
+            width: 200,
+            height: 200,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'menu.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SplashWidget());
 }
 
-class MyApp extends StatelessWidget {
+class SplashWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SplashState();
+  }
+}
+
+class SplashState extends State<SplashWidget> {
+  @override
+  void initState() {
+    super.initState();
+
+    startTime();
+  }
+
+  startTime() async {
+    var duration = new Duration(seconds: 3);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => MenuWidget()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Image.network(
-            'https://flutter.dev/assets/flutter-lockup-1caf6476beed76adec3c477586da54de6b552b2f42108ec5bc68dc63bae2df75.png',
-            width: 200,
-            height: 200,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Image.network(
+                  "https://flutter.dev/assets/flutter-lockup-1caf6476beed76adec3c477586da54de6b552b2f42108ec5bc68dc63bae2df75.png",
+                  width: 200,
+                  height: 200,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: 20.0)),
+              CircularProgressIndicator(
+                backgroundColor: Colors.white,
+                strokeWidth: 3,
+              )
+            ],
           ),
         ),
       ),
