35a5ca162235a9e5c788e325be24ee88703f4292
v.loitp <v.Loitp@cloudhms.net>
2021-02-22 23:35:04 +0700

    update

--- lib/base/widget/drawer/drawer/drawer_screen.dart
+++ lib/base/widget/drawer/drawer/drawer_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hello_word/base/const/constants.dart';
+import 'package:hello_word/base/util/ui_utils.dart';
 
 class DrawerScreen extends StatelessWidget {
   @override
@@ -11,33 +13,51 @@ class DrawerScreen extends StatelessWidget {
               title: Text('DrawerScreen'),
             ),
             body: Center(
-              child: Text('DrawerScreen'),
+              child: UIUtils().getText("DrawerScreen"),
             ),
             floatingActionButton: FloatingActionButton(
               onPressed: () {
-                Navigator.pop(context)
-                ,
+                Navigator.pop(context);
               },
-              child: Icon(Icons.backpack),
+              child: Icon(Icons.exit_to_app),
             ),
             backgroundColor: Colors.cyan,
             drawer: Drawer(
               child: ListView(
                 children: <Widget>[
                   UserAccountsDrawerHeader(
-                    accountName: Text(
-                      'flutter',
-                    ),
-                    accountEmail: Text(
-                      'freuss47@gmail.com',
-                    ),
+                    accountName: UIUtils().getText("flutter"),
+                    accountEmail: UIUtils().getText("freuss47@gmail.com"),
                   ),
                   ListTile(
-                    title: Text('Title1'),
+                    title: UIUtils().getText("Title1"),
                   ),
                   ListTile(
-                    title: Text('Title1'),
-                  )
+                    title: UIUtils().getText("Title2"),
+                  ),
+                  ListTile(
+                    title: UIUtils().getText("Title3"),
+                  ),
+                  ListTile(
+                    title: UIUtils().getText("Title4"),
+                  ),
+                  ListTile(
+                    title: UIUtils().getText("Title5"),
+                  ),
+                  UIUtils().getRaisedButton(
+                    "Click",
+                    () => Navigator.pop(context),
+                  ),
+                  Image.network(Constants.dummy_image_link),
+                  ListTile(
+                    title: UIUtils().getText("Title6"),
+                  ),
+                  ListTile(
+                    title: UIUtils().getText("Title7"),
+                  ),
+                  ListTile(
+                    title: UIUtils().getText("Title8"),
+                  ),
                 ],
               ),
             ),
