17f1fc9db797df8c640ebd1dbb6a01081885384c
v.loitp <v.Loitp@cloudhms.net>
2021-02-24 17:03:12 +0700

    update

--- lib/base/widget/layout/menu_layout_screen.dart
+++ lib/base/widget/layout/menu_layout_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hello_word/base/util/ui_utils.dart';
 import 'file:///D:/LoitppFlutter/hello_word/lib/base/widget/layout/custom_multi_child_layout/custom_multi_child_layout.dart';
+import 'package:hello_word/base/widget/layout/layout_builder_screen.dart';
 
 class MenuLayoutScreen extends StatelessWidget {
   @override
@@ -25,6 +26,17 @@ class MenuLayoutScreen extends StatelessWidget {
                 )
               },
             ),
+            UIUtils().getRaisedButton(
+              "LayoutBuilderScreen",
+                  () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => LayoutBuilderScreen()),
+                )
+              },
+            ),
+
           ],
         ),
       ),
