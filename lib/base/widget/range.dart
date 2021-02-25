168c3cc3ea8fcf766d2150f6e157a1927089c8cf
v.loitp <v.Loitp@cloudhms.net>
2021-02-25 17:37:51 +0700

    update

---
+++ lib/base/widget/range.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MySliderApp(),
+    );
+  }
+}
+
+class MySliderApp extends StatefulWidget {
+  MySliderApp({Key key}) : super(key: key);
+
+  @override
+  _MySliderAppState createState() => _MySliderAppState();
+}
+
+class _MySliderAppState extends State<MySliderApp> {
+  RangeValues _currentRangeValues = const RangeValues(20, 60);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Range Slider Demo'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Center(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                Icon(
+                  Icons.volume_up,
+                  size: 40,
+                ),
+                new Expanded(
+                    child: RangeSlider(
+                  values: _currentRangeValues,
+                  min: 0,
+                  max: 100,
+                  divisions: 10,
+                  labels: RangeLabels(
+                    _currentRangeValues.start.round().toString(),
+                    _currentRangeValues.end.round().toString(),
+                  ),
+                  onChanged: (RangeValues values) {
+                    setState(() {
+                      _currentRangeValues = values;
+                    });
+                  },
+                )),
+              ])),
+        ));
+  }
+}
