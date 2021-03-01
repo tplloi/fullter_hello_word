7058263682b0a9c0337897cbb452fccd5adf5d47
Loitp <v.loitp@cloudhms.net>
2021-03-01 13:10:31 +0700

    update

--- lib/base/widget/list/menu_list_screen.dart
+++ lib/base/widget/list/menu_list_screen.dart
@@ -5,6 +5,7 @@ import 'package:hello_word/base/widget/list/list_random_words/list_random_words.
 import 'package:hello_word/base/widget/list/list_title_widget/list_tile_screen.dart';
 
 import 'list_body_widget/list_body_screen.dart';
+import 'load_more/list_view_load_more.dart';
 
 class MenuListScreen extends StatelessWidget {
   @override
@@ -52,6 +53,15 @@ class MenuListScreen extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => ListTileScreen()),
+                )
+              },
+            ),
+            UIUtils().getRaisedButton(
+              "ListViewLoadMoreScreen",
+                  () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ListViewLoadMoreScreen()),
                 )
               },
             ),
