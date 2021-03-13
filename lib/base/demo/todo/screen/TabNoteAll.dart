4ea21346b44a90c2e01bdce58852a61f229f2528
Loitp <v.loitp@cloudhms.net>
2021-03-13 21:50:59 +0700

    update

--- lib/base/demo/todo/screen/TabNoteAll.dart
+++ lib/base/demo/todo/screen/TabNoteAll.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:hello_word/base/const/Constants.dart';
 
 class Tab1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        child: Center(
-          child: Column(
-            // center the children
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(
-                Icons.favorite,
-                size: 160.0,
-                color: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(Constants.margin_padding_large),
+            child: Text(
+              "All",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 65,
               ),
-              Text(
-                "Tab1",
-                style: TextStyle(color: Colors.white),
-              )
-            ],
+            ),
           ),
-        ),
+          ListView(
+            physics: BouncingScrollPhysics(),
+            children: [],
+          ),
+        ],
       ),
     );
   }
