e37ebd4e661e743bb356ff1feb38a85365462f02
Loitp <v.loitp@cloudhms.net>
2021-03-18 13:18:01 +0700

    update

--- lib/base/widget/horizontalDataTable/HorizontalDataTableScreen.dart
+++ lib/base/widget/horizontalDataTable/HorizontalDataTableScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hello_word/base/const/Constants.dart';
 import 'package:hello_word/base/util/UIUtils.dart';
+import 'package:horizontal_data_table/horizontal_data_table.dart';
 
 //https://pub.dev/packages/horizontal_data_table
 class HorizontalDataTableScreen extends StatelessWidget {
@@ -13,7 +13,217 @@ class HorizontalDataTableScreen extends StatelessWidget {
         () => Get.back(),
         null,
       ),
-      body: Container(),
+      body: DataTableHomePage(),
     );
   }
+}
+
+class DataTableHomePage extends StatefulWidget {
+  DataTableHomePage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _DataTableHomePageState createState() => _DataTableHomePageState();
+}
+
+class _DataTableHomePageState extends State<DataTableHomePage> {
+  HDTRefreshController _hdtRefreshController = HDTRefreshController();
+
+  static const int sortName = 0;
+  static const int sortStatus = 1;
+  bool isAscending = true;
+  int sortType = sortName;
+
+  @override
+  void initState() {
+    user.initData(100);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _getBodyWidget(),
+    );
+  }
+
+  Widget _getBodyWidget() {
+    return Container(
+      child: HorizontalDataTable(
+        leftHandSideColumnWidth: 100,
+        rightHandSideColumnWidth: 600,
+        isFixedHeader: true,
+        headerWidgets: _getTitleWidget(),
+        leftSideItemBuilder: _generateFirstColumnRow,
+        rightSideItemBuilder: _generateRightHandSideColumnRow,
+        itemCount: user.userInfo.length,
+        rowSeparatorWidget: const Divider(
+          color: Colors.black54,
+          height: 1.0,
+          thickness: 0.0,
+        ),
+        leftHandSideColBackgroundColor: Color(0xFFFFFFFF),
+        rightHandSideColBackgroundColor: Color(0xFFFFFFFF),
+        enablePullToRefresh: true,
+        refreshIndicator: const WaterDropHeader(),
+        refreshIndicatorHeight: 60,
+        onRefresh: () async {
+          //Do sth
+          await Future.delayed(const Duration(milliseconds: 500));
+          _hdtRefreshController.refreshCompleted();
+        },
+        htdRefreshController: _hdtRefreshController,
+      ),
+      height: MediaQuery.of(context).size.height,
+    );
+  }
+
+  List<Widget> _getTitleWidget() {
+    return [
+      FlatButton(
+        padding: EdgeInsets.all(0),
+        child: _getTitleItemWidget(
+            'Name' + (sortType == sortName ? (isAscending ? '↓' : '↑') : ''),
+            100),
+        onPressed: () {
+          sortType = sortName;
+          isAscending = !isAscending;
+          user.sortName(isAscending);
+          setState(() {});
+        },
+      ),
+      FlatButton(
+        padding: EdgeInsets.all(0),
+        child: _getTitleItemWidget(
+            'Status' +
+                (sortType == sortStatus ? (isAscending ? '↓' : '↑') : ''),
+            100),
+        onPressed: () {
+          sortType = sortStatus;
+          isAscending = !isAscending;
+          user.sortStatus(isAscending);
+          setState(() {});
+        },
+      ),
+      _getTitleItemWidget('Phone', 200),
+      _getTitleItemWidget('Register', 100),
+      _getTitleItemWidget('Termination', 200),
+    ];
+  }
+
+  Widget _getTitleItemWidget(String label, double width) {
+    return Container(
+      child: Text(label, style: TextStyle(fontWeight: FontWeight.bold)),
+      width: width,
+      height: 56,
+      padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+      alignment: Alignment.centerLeft,
+    );
+  }
+
+  Widget _generateFirstColumnRow(BuildContext context, int index) {
+    return Container(
+      child: Text(user.userInfo[index].name),
+      width: 100,
+      height: 52,
+      padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+      alignment: Alignment.centerLeft,
+    );
+  }
+
+  Widget _generateRightHandSideColumnRow(BuildContext context, int index) {
+    return Row(
+      children: <Widget>[
+        Container(
+          child: Row(
+            children: <Widget>[
+              Icon(
+                  user.userInfo[index].status
+                      ? Icons.notifications_off
+                      : Icons.notifications_active,
+                  color:
+                      user.userInfo[index].status ? Colors.red : Colors.green),
+              Text(user.userInfo[index].status ? 'Disabled' : 'Active')
+            ],
+          ),
+          width: 100,
+          height: 52,
+          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+          alignment: Alignment.centerLeft,
+        ),
+        Container(
+          child: Text(user.userInfo[index].phone),
+          width: 200,
+          height: 52,
+          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+          alignment: Alignment.centerLeft,
+        ),
+        Container(
+          child: Text(user.userInfo[index].registerDate),
+          width: 100,
+          height: 52,
+          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+          alignment: Alignment.centerLeft,
+        ),
+        Container(
+          child: Text(user.userInfo[index].terminationDate),
+          width: 200,
+          height: 52,
+          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+          alignment: Alignment.centerLeft,
+        ),
+      ],
+    );
+  }
+}
+
+User user = User();
+
+class User {
+  List<UserInfo> userInfo = [];
+
+  void initData(int size) {
+    for (int i = 0; i < size; i++) {
+      userInfo.add(UserInfo(
+          "User_$i", i % 3 == 0, '+001 9999 9999', '2019-01-01', 'N/A'));
+    }
+  }
+
+  ///
+  /// Single sort, sort Name's id
+  void sortName(bool isAscending) {
+    userInfo.sort((a, b) {
+      int aId = int.tryParse(a.name.replaceFirst('User_', ''));
+      int bId = int.tryParse(b.name.replaceFirst('User_', ''));
+      return (aId - bId) * (isAscending ? 1 : -1);
+    });
+  }
+
+  ///
+  /// sort with Status and Name as the 2nd Sort
+  void sortStatus(bool isAscending) {
+    userInfo.sort((a, b) {
+      if (a.status == b.status) {
+        int aId = int.tryParse(a.name.replaceFirst('User_', ''));
+        int bId = int.tryParse(b.name.replaceFirst('User_', ''));
+        return (aId - bId);
+      } else if (a.status) {
+        return isAscending ? 1 : -1;
+      } else {
+        return isAscending ? -1 : 1;
+      }
+    });
+  }
+}
+
+class UserInfo {
+  String name;
+  bool status;
+  String phone;
+  String registerDate;
+  String terminationDate;
+
+  UserInfo(this.name, this.status, this.phone, this.registerDate,
+      this.terminationDate);
 }
