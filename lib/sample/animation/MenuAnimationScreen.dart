9700ed4eeb2b2a69d1d5e3ec878305393408acff
Loitp <v.loitp@cloudhms.net>
2021-04-22 22:45:04 +0700

    update

--- lib/sample/animation/MenuAnimationScreen.dart
+++ lib/sample/animation/MenuAnimationScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hello_word/lib/common/const/DimenConstants.dart';
 import 'package:hello_word/lib/util/UIUtils.dart';
 
 import 'AnimatedAlignScreen.dart';
@@ -36,144 +37,143 @@ class MenuAnimationScreen extends StatelessWidget {
         },
         null,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            UIUtils.getButton(
-              "HeroAnimationDemoScreen",
-              () => {
-                Get.to(HeroAnimationDemoScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "RouteTransitionScreen",
-              () => {
-                Get.to(RouteTransitionScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedAlignScreen",
-              () => {
-                Get.to(AnimatedAlignScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedBuilder",
-              () => {
-                Get.to(AnimatedBuilderScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedContainerScreen",
-              () => {
-                Get.to(AnimatedContainerScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedCrossFadeScreen",
-              () => {
-                Get.to(AnimatedCrossFadeScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedDefaultTextStyleScreen",
-              () => {
-                Get.to(AnimatedDefaultTextStyleScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedIconScreen",
-              () => {
-                Get.to(AnimatedIconScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedListScreen",
-              () => {
-                Get.to(AnimatedListScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedModalBarrierScreen",
-              () => {
-                Get.to(AnimatedModalBarrierScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedOpacityScreen",
-              () => {
-                Get.to(AnimatedOpacityScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedPaddingScreen",
-              () => {
-                Get.to(AnimatedPaddingScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedPhysicalModelScreen",
-              () => {
-                Get.to(AnimatedPhysicalModelScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedPositionedScreen",
-              () => {
-                Get.to(AnimatedPositionedScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedSizeScreen",
-              () => {
-                Get.to(AnimatedSizeScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedSwitcherScreen",
-              () => {
-                Get.to(AnimatedSwitcherScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "AnimatedThemeScreen",
-              () => {
-                Get.to(AnimatedThemeScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "DecoratedBoxTransitionScreen",
-              () => {
-                Get.to(DecoratedBoxTransitionScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "FadeInImageScreen",
-              () => {
-                Get.to(FadeInImageScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "FadeTransitionScreen",
-              () => {
-                Get.to(FadeTransitionScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "PositionedTransitionScreen",
-              () => {
-                Get.to(PositionedTransitionScreen()),
-              },
-            ),
-            UIUtils.getButton(
-              "RotationTransitionScreen",
-              () => {
-                Get.to(RotationTransitionScreen()),
-              },
-            ),
-          ],
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(DimenConstants.marginPaddingMedium),
+        physics: BouncingScrollPhysics(),
+        children: [
+          UIUtils.getButton(
+            "HeroAnimationDemoScreen",
+            () => {
+              Get.to(HeroAnimationDemoScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "RouteTransitionScreen",
+            () => {
+              Get.to(RouteTransitionScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedAlignScreen",
+            () => {
+              Get.to(AnimatedAlignScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedBuilder",
+            () => {
+              Get.to(AnimatedBuilderScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedContainerScreen",
+            () => {
+              Get.to(AnimatedContainerScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedCrossFadeScreen",
+            () => {
+              Get.to(AnimatedCrossFadeScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedDefaultTextStyleScreen",
+            () => {
+              Get.to(AnimatedDefaultTextStyleScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedIconScreen",
+            () => {
+              Get.to(AnimatedIconScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedListScreen",
+            () => {
+              Get.to(AnimatedListScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedModalBarrierScreen",
+            () => {
+              Get.to(AnimatedModalBarrierScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedOpacityScreen",
+            () => {
+              Get.to(AnimatedOpacityScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedPaddingScreen",
+            () => {
+              Get.to(AnimatedPaddingScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedPhysicalModelScreen",
+            () => {
+              Get.to(AnimatedPhysicalModelScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedPositionedScreen",
+            () => {
+              Get.to(AnimatedPositionedScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedSizeScreen",
+            () => {
+              Get.to(AnimatedSizeScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedSwitcherScreen",
+            () => {
+              Get.to(AnimatedSwitcherScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "AnimatedThemeScreen",
+            () => {
+              Get.to(AnimatedThemeScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "DecoratedBoxTransitionScreen",
+            () => {
+              Get.to(DecoratedBoxTransitionScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "FadeInImageScreen",
+            () => {
+              Get.to(FadeInImageScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "FadeTransitionScreen",
+            () => {
+              Get.to(FadeTransitionScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "PositionedTransitionScreen",
+            () => {
+              Get.to(PositionedTransitionScreen()),
+            },
+          ),
+          UIUtils.getButton(
+            "RotationTransitionScreen",
+            () => {
+              Get.to(RotationTransitionScreen()),
+            },
+          ),
+        ],
       ),
     );
   }
