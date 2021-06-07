2e08b0f3d47fdd5b49d741262d339489f60034bb
Loitp <v.loitp@cloudhms.net>
2021-06-07 11:27:36 +0700

    update

--- lib/sample/widget/menu_widget_screen.dart
+++ lib/sample/widget/menu_widget_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hello_word/lib/common/const/dimen_constants.dart';
@@ -12,6 +13,7 @@ import 'package:hello_word/sample/widget/interactive_viewer/using_interactive_vi
 import 'package:hello_word/sample/widget/md2_tab_indicator/md2_tab_indicator_screen.dart';
 import 'package:hello_word/sample/widget/menu_horizontal_data_table/menu_horizontal_data_table_screen.dart';
 import 'package:hello_word/sample/widget/tab_page_selector/tab_page_selector_sreen.dart';
+import 'package:hello_word/sample/widget/video_player/video_player_screen.dart';
 
 import 'appbar/SliverAppBarScreen.dart';
 import 'button/menu_button_screen.dart';
@@ -56,257 +58,266 @@ class MenuWidgetScreen extends StatelessWidget {
         },
         null,
       ),
-      body: ListView(
-        padding: EdgeInsets.all(DimenConstants.marginPaddingMedium),
-        physics: BouncingScrollPhysics(),
-        children: [
-          UIUtils.getButton(
-            "SliverAppBarScreen",
-            () {
-              Get.to(SliverAppBarScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuBottomBarScreen",
-            () {
-              Get.to(MenuBottomBarScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "BottomSheetScreen",
-            () {
-              Get.to(BottomSheetScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuButtonScreen",
-            () {
-              Get.to(MenuButtonScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "CardScreen",
-            () {
-              Get.to(CardScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "ChartScreen",
-            () {
-              Get.to(ChartScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "CheckBoxScreen",
-            () {
-              Get.to(CheckBoxScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "RadioButtonScreen",
-            () {
-              Get.to(RadioButtonScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "RadioButtonScreen2",
-            () {
-              Get.to(RadioButtonScreen2());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuCupertinoScreen",
-            () {
-              Get.to(MenuCupertinoScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "DataTableScreen",
-            () {
-              Get.to(DataTableScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "DialogScreen",
-            () {
-              Get.to(DialogScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuDrawerScreen",
-            () {
-              Get.to(MenuDrawerScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "EasyLoadingScreen",
-            () {
-              Get.to(EasyLoadingScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuEditTextScreen",
-            () {
-              Get.to(MenuEditTextScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "ExpandedScreen",
-            () {
-              Get.to(ExpandedScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuExpansionScreen",
-            () {
-              Get.to(MenuExpansionScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "GestureScreen",
-            () {
-              Get.to(GestureScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuGridScreen",
-            () {
-              Get.to(MenuGridScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "InkwellScreen",
-            () {
-              Get.to(InkwellScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "UsingInteractiveViewerScreen",
-            () {
-              Get.to(UsingInteractiveViewerScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuLayoutScreen",
-            () {
-              Get.to(MenuLayoutScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuListScreen",
-            () {
-              Get.to(MenuListScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MD2TabIndicatorScreen",
-            () {
-              Get.to(MD2TabIndicatorScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuHorizontalDataTableScreen",
-            () {
-              Get.to(MenuHorizontalDataTableScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuImageScreen",
-            () {
-              Get.to(MenuImageScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "DayPickerScreen",
-            () {
-              Get.to(DayPickerScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuProgressScreen",
-            () {
-              Get.to(MenuProgressScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "ShimmerScreen",
-            () {
-              Get.to(ShimmerScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "MenuSliderScreen",
-            () {
-              Get.to(MenuSliderScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "StackScreen",
-            () {
-              Get.to(StackScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "StatelessWidgetDemoScreen",
-            () {
-              Get.to(StatelessWidgetDemoScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "StatefulWidgetDemoScreen",
-            () {
-              Get.to(StatefulWidgetDemoScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "StepperScreen",
-            () {
-              Get.to(StepperScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "SwitchScreen",
-            () {
-              Get.to(SwitchScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "TabPageSelectorScreen",
-            () {
-              Get.to(TabPageSelectorScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "TableScreen",
-            () {
-              Get.to(TableScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "TextAnimatedTextKitScreen",
-            () {
-              Get.to(TextAnimatedTextKitScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "TextScreen",
-            () {
-              Get.to(TextScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "TooltipScreen",
-            () {
-              Get.to(TooltipScreen());
-            },
-          ),
-          UIUtils.getButton(
-            "WebViewScreen",
-            () {
-              Get.to(WebViewScreen());
-            },
-          ),
-        ],
+      body: CupertinoScrollbar(
+        isAlwaysShown: true,
+        child: ListView(
+          padding: EdgeInsets.all(DimenConstants.marginPaddingMedium),
+          physics: BouncingScrollPhysics(),
+          children: [
+            UIUtils.getButton(
+              "SliverAppBarScreen",
+                  () {
+                Get.to(SliverAppBarScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuBottomBarScreen",
+                  () {
+                Get.to(MenuBottomBarScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "BottomSheetScreen",
+                  () {
+                Get.to(BottomSheetScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuButtonScreen",
+                  () {
+                Get.to(MenuButtonScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "CardScreen",
+                  () {
+                Get.to(CardScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "ChartScreen",
+                  () {
+                Get.to(ChartScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "CheckBoxScreen",
+                  () {
+                Get.to(CheckBoxScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "RadioButtonScreen",
+                  () {
+                Get.to(RadioButtonScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "RadioButtonScreen2",
+                  () {
+                Get.to(RadioButtonScreen2());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuCupertinoScreen",
+                  () {
+                Get.to(MenuCupertinoScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "DataTableScreen",
+                  () {
+                Get.to(DataTableScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "DialogScreen",
+                  () {
+                Get.to(DialogScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuDrawerScreen",
+                  () {
+                Get.to(MenuDrawerScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "EasyLoadingScreen",
+                  () {
+                Get.to(EasyLoadingScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuEditTextScreen",
+                  () {
+                Get.to(MenuEditTextScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "ExpandedScreen",
+                  () {
+                Get.to(ExpandedScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuExpansionScreen",
+                  () {
+                Get.to(MenuExpansionScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "GestureScreen",
+                  () {
+                Get.to(GestureScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuGridScreen",
+                  () {
+                Get.to(MenuGridScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "InkwellScreen",
+                  () {
+                Get.to(InkwellScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "UsingInteractiveViewerScreen",
+                  () {
+                Get.to(UsingInteractiveViewerScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuLayoutScreen",
+                  () {
+                Get.to(MenuLayoutScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuListScreen",
+                  () {
+                Get.to(MenuListScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MD2TabIndicatorScreen",
+                  () {
+                Get.to(MD2TabIndicatorScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuHorizontalDataTableScreen",
+                  () {
+                Get.to(MenuHorizontalDataTableScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuImageScreen",
+                  () {
+                Get.to(MenuImageScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "DayPickerScreen",
+                  () {
+                Get.to(DayPickerScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuProgressScreen",
+                  () {
+                Get.to(MenuProgressScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "ShimmerScreen",
+                  () {
+                Get.to(ShimmerScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "MenuSliderScreen",
+                  () {
+                Get.to(MenuSliderScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "StackScreen",
+                  () {
+                Get.to(StackScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "StatelessWidgetDemoScreen",
+                  () {
+                Get.to(StatelessWidgetDemoScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "StatefulWidgetDemoScreen",
+                  () {
+                Get.to(StatefulWidgetDemoScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "StepperScreen",
+                  () {
+                Get.to(StepperScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "SwitchScreen",
+                  () {
+                Get.to(SwitchScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "TabPageSelectorScreen",
+                  () {
+                Get.to(TabPageSelectorScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "TableScreen",
+                  () {
+                Get.to(TableScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "TextAnimatedTextKitScreen",
+                  () {
+                Get.to(TextAnimatedTextKitScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "TextScreen",
+                  () {
+                Get.to(TextScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "TooltipScreen",
+                  () {
+                Get.to(TooltipScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "VideoPlayerScreen",
+                  () {
+                Get.to(VideoPlayerScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "WebViewScreen",
+                  () {
+                Get.to(WebViewScreen());
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
