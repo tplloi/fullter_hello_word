9ad891762930208e028ee02aa75a0a6eaec42ece
Loitp <v.loitp@cloudhms.net>
2021-06-07 11:40:04 +0700

    update

--- lib/sample/widget/video_player/video_player_screen.dart
+++ lib/sample/widget/video_player/video_player_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hello_word/sample/widget/video_player/player_video_and_pop_page.dart';
 import 'package:hello_word/sample/widget/video_player/tab/bumble_bee_remote_video.dart';
 import 'package:hello_word/sample/widget/video_player/tab/butter_fly_asset_video.dart';
+import 'package:hello_word/sample/widget/video_player/tab/butter_fly_asset_video_in_list.dart';
 
 class VideoPlayerScreen extends StatelessWidget {
   @override
@@ -43,87 +44,9 @@ class VideoPlayerScreen extends StatelessWidget {
           children: <Widget>[
             BumbleBeeRemoteVideo(),
             ButterFlyAssetVideo(),
-            _ButterFlyAssetVideoInList(),
+            ButterFlyAssetVideoInList(),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class _ButterFlyAssetVideoInList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        _ExampleCard(title: "Item a"),
-        _ExampleCard(title: "Item b"),
-        _ExampleCard(title: "Item c"),
-        _ExampleCard(title: "Item d"),
-        _ExampleCard(title: "Item e"),
-        _ExampleCard(title: "Item f"),
-        _ExampleCard(title: "Item g"),
-        Card(
-            child: Column(children: <Widget>[
-          Column(
-            children: <Widget>[
-              const ListTile(
-                leading: Icon(Icons.cake),
-                title: Text("Video video"),
-              ),
-              Stack(
-                  alignment: FractionalOffset.bottomRight +
-                      const FractionalOffset(-0.1, -0.1),
-                  children: <Widget>[
-                    ButterFlyAssetVideo(),
-                    Image.asset('assets/images/flutter-mark-square-64.png'),
-                  ]),
-            ],
-          ),
-        ])),
-        _ExampleCard(title: "Item h"),
-        _ExampleCard(title: "Item i"),
-        _ExampleCard(title: "Item j"),
-        _ExampleCard(title: "Item k"),
-        _ExampleCard(title: "Item l"),
-      ],
-    );
-  }
-}
-
-/// A filler card to show the video in a list of scrolling contents.
-class _ExampleCard extends StatelessWidget {
-  const _ExampleCard({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            leading: const Icon(Icons.airline_seat_flat_angled),
-            title: Text(title),
-          ),
-          ButtonBar(
-            children: <Widget>[
-              TextButton(
-                child: const Text('BUY TICKETS'),
-                onPressed: () {
-                  /* ... */
-                },
-              ),
-              TextButton(
-                child: const Text('SELL TICKETS'),
-                onPressed: () {
-                  /* ... */
-                },
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
