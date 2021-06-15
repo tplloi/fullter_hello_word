32f3475e716da383add9a9b46ce0ced2bd6d95fa
Loitp <v.loitp@cloudhms.net>
2021-06-15 20:50:07 +0700

    update

--- lib/sample/widget/slider/menu_slider_screen.dart
+++ lib/sample/widget/slider/menu_slider_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hello_word/lib/common/const/dimen_constants.dart';
 import 'package:hello_word/lib/util/uI_utils.dart';
+import 'package:hello_word/sample/widget/slider/flutter_slidable/flutter_slidable_screen.dart';
 
 import 'range/range_slider_screen.dart';
 import 'slider/slider_screen.dart';
@@ -23,6 +24,12 @@ class MenuSliderScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            UIUtils.getButton(
+              "FlutterSlidableScreen",
+              () {
+                Get.to(FlutterSlidableScreen());
+              },
+            ),
             UIUtils.getButton(
               "RangeSliderScreen",
               () {
