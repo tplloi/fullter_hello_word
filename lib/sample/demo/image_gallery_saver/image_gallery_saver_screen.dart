8be497d4bec5cdf3e10747ff97912b372847b9c3
tplloi <TPLdev0402dev>
2021-06-28 19:31:15 +0700

    update

---
+++ lib/sample/demo/image_gallery_saver/image_gallery_saver_screen.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:battery_plus/battery_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hello_word/lib/common/const/dimen_constants.dart';
+import 'package:hello_word/lib/core/base_stateful_state.dart';
+import 'package:hello_word/lib/util/uI_utils.dart';
+import 'package:hello_word/lib/util/url_launcher_utils.dart';
+
+class ImageGallerySaverScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ImageGallerySaverScreenState();
+  }
+}
+
+class _ImageGallerySaverScreenState extends BaseStatefulState<ImageGallerySaverScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: UIUtils.getAppBar(
+        "ImageGallerySaverScreen",
+        () {
+          Get.back();
+        },
+        () {
+          UrlLauncherUtils.launchInWebViewWithJavaScript(
+              "https://pub.dev/packages/image_gallery_saver");
+        },
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(DimenConstants.marginPaddingMedium),
+        physics: BouncingScrollPhysics(),
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
