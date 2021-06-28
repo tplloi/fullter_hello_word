02c505cb5e5514a495871017388bcd908605cbdb
tplloi <TPLdev0402dev>
2021-06-28 19:47:58 +0700

    update

--- lib/sample/demo/image_gallery_saver/image_gallery_saver_screen.dart
+++ lib/sample/demo/image_gallery_saver/image_gallery_saver_screen.dart
@@ -124,7 +124,7 @@ class _ImageGallerySaverScreenState
 
   _getHttp() async {
     var response = await Dio().get(
-        "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a62e824376d98d1069d40a31113eb807/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg",
+        StringConstants.URL_IMG_1,
         options: Options(responseType: ResponseType.bytes));
     final result = await ImageGallerySaver.saveImage(
         Uint8List.fromList(response.data),
